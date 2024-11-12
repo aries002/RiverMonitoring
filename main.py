f5ebbc8c1c084a2984a62cab0f5eb08c07e11b23
alfian <alfianari@umsida.ac.id>
2024-11-12 16:08:33 +0700

    pindah ke system

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import time
 import water_level_detector
 import system
-import video_sender
 
 # Load config
-sys = system.system('./config.conf')
+sys = system.config('./config.conf')
 config = sys._config
 
 if __name__ == '__main__':
@@ -16,7 +15,7 @@ if __name__ == '__main__':
     sensor1_lebar_kanan = int(config['sensor1']['sensor1_lebar_kanan'])
     sensor1 = water_level_detector.water_level(videolink, sensor1_tinggi_atas, sensor1_tinggi_bawah,sensor1_lebar_kiri,sensor1_lebar_kanan) # penggil sensor
     stream_port = int(config['video_server']['port'])
-    streamer = video_sender.video_sender(config['sensor1']['videolink'],config['video_server']['address'],stream_port)
+    streamer = system.video_sender(config['sensor1']['videolink'],config['video_server']['address'],stream_port)
     
     # sensor1.thread.start() # mulai sensor
     print("Starting services")
--- system.py
+++ system.py
@@ -1,12 +1,16 @@
 import os
 import configparser
-
-
+import requests
+import cv2, imutils, socket
+import numpy as np
+import base64
+import threading
+import time
 DEF_LOCATION = ""
 DEF_TEMPLATE = './config_template.conf'
 
 
-class system:
+class config:
     def __init__(self, config_location = ''):
         self.def_if = 'testing'
         self.config_location =config_location
@@ -18,7 +22,7 @@ class system:
         self._config.read(config_location)
     
     # buat file konfigurasi
-    def create_conf(self):
+    def create(self):
         new_config = configparser.ConfigParser()
         if not os.path.isfile(DEF_TEMPLATE):
             print("Template not found exiting")
@@ -28,8 +32,12 @@ class system:
                 new_config.write(configfile)
                 configfile.close()
 
+    # simpan konfigurasi
+    def save(self):
+        pass
+
     # print konfigurasi
-    def print_config(self):
+    def print(self):
         #  data in self.data:?
         for section in self._config.sections():
             print('[',section,']')
@@ -37,6 +45,101 @@ class system:
                 print(config, "=", self._config[section][config],'(',type(self._config[section][config]),')')
         # print(self._config.sections())
     
-    # ambil data konfigurasi dari server
-    def get_config(self):
+    # download data konfigurasi dari server
+    def download(self):
         pass
+
+class api_client:
+    def __init__(self,url):
+        self.url = url
+        self.uri = {
+            'image' : '',
+            'status' : '',
+            'sensor_data' : '',
+            'recent_image' : ''
+        }
+        self.header ={}
+        pass
+
+    def get_status(self):
+        url = self.url+self.uri['status']
+        header = self.header
+
+        result = requests.get(url, headers=header)
+        return result.text
+    
+    def send_sensor(self,data):
+        url = self.url+self.uri['sensor_data']
+
+        result = requests.post(url, headers=self.header, json=data)
+        
+        return result
+    
+    def send_image(self,base64_img, url = None):
+        if url is None:
+            url = self.url+self.uri['image']
+
+        result = requests.post(url,headers=self.header,data=base64_img)
+
+        return result
+    
+    def send_recent_video_img(self,vid_id, file_name = "file", url = None):
+        if url is None:
+            url = self.url+self.uri['recent_image']
+        
+        cap = cv2.VideoCapture(vid_id)
+        if cap.isOpened():
+            res, image = cap.read()
+            if image is None:
+                return False
+        else:
+            return False
+        file = {file_name:image}
+        
+        result = requests.post(url, headers=self.header, files=file)
+
+        return result
+
+
+class video_sender:
+    def __init__(self,video_id,server_ip,server_port):
+        self.debug = False
+        self.video_res = (640,480)
+        self.buffer_size = 65536
+        self.video_id = video_id
+        self.server = (server_ip,server_port)
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,self.buffer_size)
+        self.daemon = True
+        self.run = True
+        pass
+    
+    def start(self):
+        self.thread = threading.Thread(target=self.video_stream)
+        self.thread.daemon = self.daemon
+        self.thread.run()
+
+    def video_stream(self):
+        while True:
+            if self.run:
+                cap = cv2.VideoCapture(self.video_id)
+                print("Streaming to ",self.server)
+                
+                while cap.isOpened() and self.run:
+                    # ambil frame
+                    ret, frame = cap.read()
+                    # persiapkan video untuk dikirim
+                    frame = cv2.resize(frame,self.video_res)
+                    encoded, buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+                    # rubah frame gambar menjadi base64
+                    message = base64.b64encode(buffer)
+                    # kirim frame gambar
+                    self.socket.sendto(message,self.server)
+                    # untuk debug
+                    if self.debug:
+                        cv2.imshow("Video transmited", frame)
+                        if cv2.waitKey(1) == ord('q'):
+                            break
+                cap.release()
+            else:
+                time.sleep(0.5)
