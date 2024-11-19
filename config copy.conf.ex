2de7e448a79bdd2024157617e81bdf5b298c2e3d
ARIES002 <alfianputra19@gmail.com>
2024-11-19 22:25:36 +0700

    a

---
+++ config copy.conf.ex
@@ -0,0 +1,31 @@
+[general]
+name = testing 123
+lokasi = tanggulangin
+server_auth_key = 12345
+server_address = localhost
+system_loop_interval = 1
+
+[video_server]
+address = localhost
+port = 8000
+camera = camera1
+
+[camera1]
+link = rtsp://admin:p4ssw0rdAMAN@192.168.77.179:554/Streaming/channels/102
+
+[camera2]
+link = rtsp://admin:p4ssw0rdAMAN@192.168.77.179:554/Streaming/channels/101
+
+[sensor1]
+camera = camera1
+tinggi_atas = 5
+tinggi_bawah = 600
+lebar_kiri = 0
+lebar_kanan = 43
+
+[sensor2]
+camera = camera2
+tinggi_atas = 5
+tinggi_bawah = 600
+lebar_kiri = 0
+lebar_kanan = 43
--- main.py
+++ main.py
@@ -6,19 +6,40 @@ import system
 sys = system.config('./config.conf')
 config = sys._config
 
+def sensor_setup(sensor):
+    sensor_camrea = config[sensor]['camera']
+    camera_link = config[sensor_camrea]['link']
+
+    tinggi_atas = int(config[sensor]['tinggi_atas'])
+    tinggi_bawah = int(config[sensor]['tinggi_bawah'])
+    lebar_kiri = int(config[sensor]['lebar_kiri'])
+    lebar_kanan = int(config[sensor]['lebar_kanan'])
+
+    return water_level_detector.water_level(camera_link, tinggi_atas,tinggi_bawah,lebar_kiri,lebar_kanan)
+
+def system_loop():
+    # ambil data sensor
+    # ambil data yang paling sering keluar dalam satu periode loop
+    # kirimkan data sensor
+    # ambil status dan perintah
+    # jalankan perintah
+    pass
+
+def system_setup():
+    pass
+    
+
 if __name__ == '__main__':
-    # sensor1_conf = config['sensor1']
-    videolink = config['sensor1']['videolink']
-    sensor1_tinggi_atas = int(config['sensor1']['sensor1_tinggi_atas'])
-    sensor1_tinggi_bawah = int(config['sensor1']['sensor1_tinggi_bawah'])
-    sensor1_lebar_kiri = int(config['sensor1']['sensor1_lebar_kiri'])
-    sensor1_lebar_kanan = int(config['sensor1']['sensor1_lebar_kanan'])
-    sensor1 = water_level_detector.water_level(videolink, sensor1_tinggi_atas, sensor1_tinggi_bawah,sensor1_lebar_kiri,sensor1_lebar_kanan) # penggil sensor
+    # sensor setup
+    sensor1 = sensor_setup('sensor1')
+    # video stream setup
     stream_port = int(config['video_server']['port'])
-    streamer = system.video_sender(config['sensor1']['videolink'],config['video_server']['address'],stream_port)
+    stream_camera = config['video_server']['camera']
+    streamer = system.video_sender(config[stream_camera]['link'],config['video_server']['address'],stream_port)
     
     # sensor1.thread.start() # mulai sensor
     print("Starting services")
+    # sensor start
     sensor1.start()
 
     # stream video to server
