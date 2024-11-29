68aab8184214dadcde06b9a9c6b4220c0298ee4d
ARIES002 <alfianputra19@gmail.com>
2024-11-29 07:56:07 +0700

    client fix

--- Server/api/helper.php
+++ Server/api/helper.php
@@ -7,11 +7,20 @@ $databaseUsername   = $CONFIG['mysql']['username'];
 $databasePassword   = $CONFIG['mysql']['password'];
 $koneksi = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 
+function check_token($dev_id = '', $key = '') : bool {
+    if($dev_id == '' || $key == ''){
+        return FALSE;
+    }else{
+        $result = 
+    }
+}
+
 function proses_sensor_data($sensor_data){
     if($sensor_data == null){
         return;
     }
-
+    $data = json_decode($sensor_data)
+    $result = mysqli_query($koneksi, "INSERT INTO sensor (dev_id, ")
 }
 
 function proses_pesan($pesan){
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import time
 import water_level_detector
 import system
 import argparse
+from statistics import mode
+import json
+import sys, getopt
 
-# Load config
-sistem = system.config('./config.conf')
-config = sistem._config
-api =
+
+FILE_KONFIGURASI = "./config.conf"
+DEBUG = False
+
 def sensor_setup(sensor):
     sensor_camrea = config[sensor]['camera']
     camera_link = config[sensor_camrea]['link']
@@ -18,31 +21,38 @@ def sensor_setup(sensor):
 
     return water_level_detector.water_level(camera_link, tinggi_atas,tinggi_bawah,lebar_kiri,lebar_kanan)
 
-def system_loop():
-    # ambil data sensor
-    # ambil rata rata nilai sensor dalam 1 menit
-    # ambil data yang paling sering keluar dalam satu periode loop
-    # kirimkan data sensor
-
-    # ambil status dan perintah
-    # jalankan perintah
-    pass
-
-def system_setup():
-    pass
-    
-
 if __name__ == '__main__':
-    argumen_parser = argparse.ArgumentParser()
-    argumen_parser.add_argument('-d','--debug',help="Activate debugger for development")
-    argumen = argumen_parser.parse_args()
+    ARGList = sys.argv[1:]
+    # Options
+    options = "hf:d"
+    long_options = ["Help", "file_konfig", "debug"]
+    try:
+        arguments, values = getopt.getopt(ARGList, options, long_options)
+        for currentArgument, currentValue in arguments:
+            if currentArgument in ("-h", "--Help"):
+                print("help")
+                sys.exit()
+            elif currentArgument in ("-f", "--file_konfig"):
+                FILE_KONFIGURASI = currentValue
+                print("Menggunakan file konfigurasi di "+FILE_KONFIGURASI)
+            elif currentArgument in ("-d", "--debug"):
+                DEBUG = True
+    except getopt.error as err:
+        print(str(err))
+        sys.exit()
+    # Load config
+    sistem = system.config(FILE_KONFIGURASI)
+    config = sistem._config
+    api = system.api_client(config['general']['server_address'])
     
     # sensor setup
     sensor1 = sensor_setup('sensor1')
+    sensor1.debug = DEBUG
     # video stream setup
     stream_port = int(config['video_server']['port'])
     stream_camera = config['video_server']['camera']
     streamer = system.video_sender(config[stream_camera]['link'],config['video_server']['address'],stream_port)
+    streamer.debug = DEBUG
     
     # sensor1.thread.start() # mulai sensor
     print("Starting services")
@@ -52,11 +62,34 @@ if __name__ == '__main__':
     # stream video to server
     streamer.start()
     # main process
+    detik = 0
+    tampung_sensor = []
     while True:
-        print("tinggi permukaan =",sensor1.result)
-        system_loop()
-        # if(input()) == 'q':
-        #     break
-        time.sleep(2)
-# sensor1 = water_level_detector.water_level(videolink, sensor1_tinggi_atas, sensor1_tinggi_bawah,sensor1_lebar_kiri,sensor1_lebar_kanan)
-# sensor1.image_look()
+        # print("tinggi permukaan =",sensor1.result)
+        # ambil data sensor dan kumpulkan selama 1 menit
+        if detik < 60:
+            tampung_sensor[detik] = sensor1.result # ambil data setiap detik
+            detik+1
+        else:
+            detik = 0
+            payload = []
+            payload['water_sensor'] = mode(tampung_sensor) # ambil data yang paling sering muncul untuk menghilangkan gangguan
+            json_payload = json.dumps(payload) # persiapkan data dalam json
+            api.send_sensor(json_payload) # kirimkan data
+            if DEBUG :
+                print("Sensor Data:")
+                print(tampung_sensor)
+                print(json_payload)
+            tampung_sensor = []
+        
+        # ambil status dan terapkan perintah
+        perintah = api.get_status()
+        if perintah == "start_video_stream":
+            streamer.run = True
+        elif perintah == "stop_video_stream":
+            streamer.run = False
+        elif perintah == "restart_system"+config["general"]["dev_id"]:
+            print("System rebooting in short time")
+            time.sleep(3)
+        
+        time.sleep(1)
